8afc5f23aee7edd13a79bb76c5d53a24d4966c8e
SavinNik <nikolus5000@gmail.com>
2025-10-22 18:18:29 +0300

    Fixed the display of the title and short description

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.shortcuts import render
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 def home_view(request):
@@ -10,3 +12,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home')
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
